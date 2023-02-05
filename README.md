48800610ff7bc5e963c6bb7feb68f60b90f166f7
TomJohnH <thasiow@onet.pl>
2023-02-05 13:22:01 +0100

    work in progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# streamlit-abtests
+# Streamlit a/b tests helper tool
+
+Small tool for product managers to deep dive into a/b tests universe
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import scipy
 from scipy import stats
 import streamlit as st
 import pandas as pd
+import plotly
 
 # ----- important notes -----
 
@@ -14,25 +15,54 @@ import pandas as pd
 p_values = []
 differences = []
 
+
+def diffprop(obs):
+    """
+    `obs` must be a 2x2 numpy array.
+
+    Returns:
+    delta
+        The difference in proportions
+    ci
+        The Wald 95% confidence interval for delta
+    corrected_ci
+        Yates continuity correction for the 95% confidence interval of delta.
+    """
+    n1, n2 = obs.sum(axis=1)
+    prop1 = obs[0, 0] / n1
+    prop2 = obs[1, 0] / n2
+    delta = prop1 - prop2
+
+    # Wald 95% confidence interval for delta
+    se = np.sqrt(prop1 * (1 - prop1) / n1 + prop2 * (1 - prop2) / n2)
+    ci = (delta - 1.96 * se, delta + 1.96 * se)
+
+    # Yates continuity correction for confidence interval of delta
+    correction = 0.5 * (1 / n1 + 1 / n2)
+    corrected_ci = (ci[0] - correction, ci[1] + correction)
+
+    return delta, ci, corrected_ci
+
+
 st.subheader("Chi-squared a/b test")
 
 with st.form("my_form"):
-    st.write("Please provide test results")
+    st.subheader("Please provide test results")
     col1, col2 = st.columns(2)
     with col1:
-        a_click = float(st.text_input("Base # successes", value=1513))
+        a_click = float(st.text_input("Base population # successes", value=1513))
     with col2:
-        a_population = float(st.text_input("Base # trials", value=15646))
+        a_population = float(st.text_input("Base population # trials", value=15646))
     col1, col2 = st.columns(2)
     with col1:
-        b_click_init = float(st.text_input("Test # successes", value=1553))
+        b_click_init = float(st.text_input("Test population # successes", value=1553))
     with col2:
-        b_population = float(st.text_input("Test # trials", value=15130))
+        b_population = float(st.text_input("Test population # trials", value=15130))
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.write("**Results**")
+        st.subheader("**Results**")
         st.write("Base population conversion: " + f"{(a_click / a_population):.2%}")
         st.write(
             "Test population conversion: " + f"{(b_click_init / b_population):.2%}"
@@ -45,11 +75,13 @@ with st.form("my_form"):
         T = np.array([[a_click, a_noclick], [b_click, b_noclick]])
         p_val = scipy.stats.chi2_contingency(T, correction=False)[1]
         if p_val <= 0.05:
-            sig_test = " ...significant!"
+            sig_test = ".<br> P-value lower than 0.05. Result is statistically significant, therefore you can with 95% probabliti reject the hyphotesis that conversions do not differ."
+            color = "green"
         else:
-            sig_test = " ...not significant!"
+            sig_test = ".<br> P-value greater than 0.05. Result is not statistically significant, therefore you cannot with 95% probablitiy reject the hyphotesis that conversions do not differ."
+            color = "red"
         st.markdown(
-            "<span style='color:green'>Difference: "
+            f"<span style='color:{color}'>Difference: "
             + f"{(a_click / a_population - b_click_init / b_population):.2%}"
             + " p-value "
             + str(f"{p_val:.4f}")
@@ -57,7 +89,7 @@ with st.form("my_form"):
             + "</span>",
             unsafe_allow_html=True,
         )
-
+        st.subheader("Additional information")
         st.write(
             "**Check the value of difference when the chi-squared test would be significant**"
         )
@@ -65,7 +97,7 @@ with st.form("my_form"):
 
             a_noclick = a_population - a_click
 
-            b_click = b_click_init + i
+            b_click = b_click_init + round(i * (b_click_init / 1000))
             b_noclick = b_population - b_click
 
             T = np.array([[a_click, a_noclick], [b_click, b_noclick]])
@@ -75,7 +107,19 @@ with st.form("my_form"):
 
         df = pd.DataFrame({"Difference": differences, "p-values": p_values})
         df_styled = df.style.format({"Difference": "{:.4%}", "p-values": "{:.4%}"})
-
         st.dataframe(df_styled)
+
+        st.write("**Closer look to the significance border**")
+
+        df2 = df[df["p-values"] > 0.045]
+        df2 = df2[df2["p-values"] < 0.055]
+        df2_styled = df2.style.format({"Difference": "{:.4%}", "p-values": "{:.4%}"})
+        st.dataframe(df2_styled)
+
+        st.write("**P-value (y-axis) vs \% diffrence (x-axis) chart**")
         df.Difference = df.Difference * 100
         st.line_chart(df.set_index("Difference"))
+
+        K = np.array([[a_click, a_noclick], [b_click_init, b_noclick]])
+
+        st.write(diffprop(K))
